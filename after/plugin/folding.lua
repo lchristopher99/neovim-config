43050a3cea7f8ab728a11e1124755e2f2f4bcebe
logan <l.christopher@martinfed.com>
2023-08-03 09:54:49 -0500

    - added code folding keymaps

--- after/plugin/folding.lua
+++ after/plugin/folding.lua
@@ -2,3 +2,6 @@ vim.api.nvim_create_autocmd({"BufReadPost", "FileReadPost"}, {
     pattern = {"*"},
     command = "normal zR"
 })
+
+vim.keymap.set("n", "ff", "zc")
+vim.keymap.set("n", "df", "zR")
